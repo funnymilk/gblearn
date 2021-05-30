be44c8c4993811293ad2b189db7e8027fb6c4234
funnymilk <andre.tum91@gmail.com>
2021-05-30 10:20:21 +0300

    finihed ex9

--- algorithm/ex9/ex9/main.c
+++ algorithm/ex9/ex9/main.c
@@ -1,10 +1,76 @@
 #include <stdio.h>
+#include <stdlib.h>
 
 #define T int
 #define SIZE 1000
 #define true 1 == 1
 #define false 1 != 1
+#define sz 10
 typedef int boolean;
+typedef struct{
+    int pr;
+    int dat;
+} Item;
+
+Item* arr[sz];
+int head;
+int tail;
+int items;
+
+void init(){
+    for (int i = 0; i < sz; ++i){
+        arr[i] = NULL;
+    }
+    head = 0;
+    tail = 0;
+    items = 0;
+}
+
+void ins(int pr, int dat){
+    Item *item = (Item*) malloc(sizeof(Item));
+    item->dat = dat;
+    item->pr = pr;
+    if (items < sz) {
+        arr[tail++] = item;
+        items++;
+    } else if (items == sz){
+        printf("%s \n", "Queue is full");
+        return;
+    }
+}
+
+Item* rem(int pr){
+    int i = 0;
+    if (items == 0){
+        return NULL;
+    } else {
+        Item *tmp;
+        while (i <= tail-1){
+            if (arr[i]->pr == pr){
+                tmp = arr[i];
+               for(int j = i; j <= tail-2; j++){
+                    arr[j] = arr[j+1];
+                }
+                arr[tail-1] = NULL;
+                items--;
+                tail--;
+                return arr[tail];
+            }
+            i++;
+        }
+    }
+}
+
+void printQueue(){
+    printf("[");
+    for (int i = 0; i < sz-1; i++){
+        if (arr[i] == NULL)
+            printf("[*, *] ");
+        else
+            printf("[%d, %d] ", arr[i]->pr, arr[i]->dat);
+    }
+    printf("]\n");
+}
 
 int cursor = -1;
 T Stack[SIZE];
@@ -37,6 +103,31 @@ int main()
         printf("%d ", pop());
     }
     printf("\n");
+    printf("----------------------------------\n");
+
+    init();
+    ins(1, 1);
+    ins(4, 22);
+    ins(5, 23);
+    ins(1, 223);
+    ins(2, 0);
+    ins(4, 43);
+    ins(7, 12);
+    ins(2, 15);
+    ins(9, 11);
+    printQueue();
+
+    for (int i = 0; i < tail; i++){
+        rem(2);
+
+    }
+    printQueue();
+
+
+    ins(1, 1);
+    printQueue();
+
+
     return 0;
 }
 
